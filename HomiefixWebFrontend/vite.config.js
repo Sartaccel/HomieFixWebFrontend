b62643658d74ff7320b33febdd80c6fed7fd6841
Subin Sundar B <subinbsundar1211@gmail.com>
2025-07-16 10:35:45 +0530

    Update vite.config.js

--- HomiefixWebFrontend/vite.config.js
+++ HomiefixWebFrontend/vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ["@coreui/react"],
+    include: ['@coreui/react'],
   },
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true,
+  },
 })
